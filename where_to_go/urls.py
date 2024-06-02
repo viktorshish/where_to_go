a47b596a4538f1864f67d0492257d347644f675b
Shishkin PRO <viktorshish@gmail.com>
2024-06-02 22:17:02 +0300

    Init django project and add blank start page

---
+++ where_to_go/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+
+from where_to_go import views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.show_index_page)
+]
